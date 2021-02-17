e26f7ab6bd44a98124dc15c64d4e0f65c090e5f8
Salman Zafar <salman.zafar@innowi.com>
2021-02-17 13:49:13 +0500

    optional query paramter in index method

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
 @app.get('/')
-def index(limit):
-    return {
-        'data': f'{limit} from the blog List'
-    }
+def index(limit: int = 10, published: bool = True, sort:Optional[str]=None):
+
+    return sort
+
+    if published:
+        return {
+            'data': f'{limit} published blogs'
+        }
+    else:
+        return {
+            'data': f'{limit} from the blog List'
+        }
 
 
 @app.get('/blog/unpublished')
@@ -18,7 +27,7 @@ def unpublished_blogs():
 
 
 @app.get('/blog/{id}')
-def show(id:int):
+def show(id: int):
     return {
         'data': id
     }
