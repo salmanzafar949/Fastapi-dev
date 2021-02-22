2fcba0ba9d3bb6265c5918b09cdb0487a41c7277
Salman Zafar <salman.zafar@innowi.com>
2021-02-22 14:48:05 +0500

    Minor changes in model.py

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 class Blog(Base):
     __tablename__ = 'blogs'
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer,ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
     title = Column(String)
     body = Column(String)
     published = Column(Boolean, default=True)
@@ -19,6 +19,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     email = Column(Integer, unique=True)
-    password = Column(Integer)
+    password = Column(String)
 
     blogs = relationship('Blog', back_populates="author")
